49ac1b66b50a41500dbede84dd74316436c4d5da
felipeti <felipe.marquestech@gmail.com>
2014-10-08 21:41:41 -0300

    atualização

---
+++ src/main/java/br/com/fatec/cinetech/entity/Form.java
@@ -0,0 +1,28 @@
+package br.com.fatec.cinetech.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Form {
+	@Id
+	private Long id;
+	private String nome;
+	private String sobrenome;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+}
